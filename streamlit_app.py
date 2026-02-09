9be572395818c5b9ef681745f198f8acea66c0c6
luispargs <85448752+luispargs@users.noreply.github.com>
2026-02-09 21:35:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -8,7 +9,6 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!
   """
 )
-
 #code for name
 name_on_order=st.text_input('Name on Smoothie:')
 st.write('The name of your smoothie will be', name_on_order)
@@ -18,9 +18,6 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-
-
-
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:', 
     my_dataframe,
@@ -28,22 +25,18 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
-
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
     ingredients_string =''
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += (fruit_chosen + ' ')
+      ingredients_string += (fruit_chosen + ' ')
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
         
-    
-
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """' , '""" + name_on_order+"""')"""
-    #st.write(ingredients_string)
- #   st.write(my_insert_stmt)
-    #st.stop()
-    
+  
     time_to_insert = st.button('Submit Order')
     
     if time_to_insert:
@@ -51,7 +44,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
